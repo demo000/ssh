18e1e440d77b69366ee991dac761c25216b0a0f3
dem <dem@gmail.com>
2017-02-02 15:53:57 +0800

    SSH空项目

---
+++ src/main/java/com/dem/domain/Person.java
@@ -0,0 +1,13 @@
+package com.dem.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 创建人: DEM .
+ */
+@Setter@Getter
+public class Person {
+    private String id;
+    private String name;
+}
